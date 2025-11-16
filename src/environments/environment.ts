856b431482602bf93c6ef28ee6f1f0db6ff3e871
shubham <AL1153123@admin.hubtotal.net>
2025-11-17 00:11:59 +0530

    Added redirected URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   azureAd: {
     clientId: 'be1d6c59-a164-40fb-a8ec-b9d828df8376',
     tenantId: '3d36a99a-907d-43c5-9785-e58985e7e62b',
-    redirectUri: 'http://localhost:4200',
+    redirectUri: 'https://weather-app-api-aqgudregchgqbvej.centralus-01.azurewebsites.net/',
   }
 };
